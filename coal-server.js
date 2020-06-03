5621cf12ecadec60796ca26a2df836582e391d4b
FinnWalker <finn3walker@gmail.com>
2020-06-03 18:03:15 +0100

    test

--- coal-server.js
+++ coal-server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 app.use("/", express.static("dist"));
+app.use("/test", express.static("website-demo"));
 
 const port = 5445;
 app.listen(port, "0.0.0.0", () => {
