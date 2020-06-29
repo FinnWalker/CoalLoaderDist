cba76b25699639af41789ab6eb4079fa01850ac6
FinnWalker <finn3walker@gmail.com>
2020-06-29 15:14:38 +0100

    test

--- coal-server.js
+++ coal-server.js
@@ -9,7 +9,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 app.use(cors());
-
+app.options('*', cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/", express.static("dist"));
